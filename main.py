bfcf982ed5099d6b1b21d6da93d1249ad231b293
Bogdan Boboc <bogdanboboc97@gmail.com>
2017-10-28 13:14:01 +0000

    add motion sensor check

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from flask import Flask
-from threading import Threading,Lock
-
+from threading import Thread,Lock
+import RPi.GPIO as GPIO
+import time
 app = Flask(__name__)
 
+# GPIO configuration
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+INPUT_GPIO = 37
+GPIO.setup(INPUT_GPIO, GPIO.IN)
+
+# movement stuff
 last_movement=0
 mutex = Lock()
-DEFAULT_TIME_OUT = 5000
+DEFAULT_TIME_OUT = 300
 
 @app.route("/get_movement")
 def get_movement():
 	global last_movement,mutex
 
 	mutex.acquire()
-	time_diff = time.time() - last_movement()
+	time_diff = time.time() - last_movement
+	print(time_diff)
 	mutex.release()
 
 	if time_diff <= DEFAULT_TIME_OUT:
@@ -23,15 +32,17 @@ def get_movement():
 	#return flag
 
 def check_movement():
-	global last_movement,mutex
-	i = 0
-	while True:
-		if i == 1:
-			mutex.acquire()
-			last_movement = time.time()
-			mutex.release()
-
-		time.sleep(0.1)
+        global last_movement,mutex,GPIO
+        i = 0
+        while True:
+                i = GPIO.input(INPUT_GPIO)
+                if i == 1:
+                        mutex.acquire()
+                        print("Rick")
+                        last_movement = time.time()
+                        mutex.release()
+                
+                time.sleep(0.1)
 
 def start_service():
 	app.run(host='0.0.0.0')
